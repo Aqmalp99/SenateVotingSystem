f3ab4cfdaa1eba6c47f74eeb3f085aadfbc0c045
Mohmad Aqmal Pulle <a1720825@student.adelaide.edu.au>
2022-05-27 21:07:03 +0930

    my rspec is failing as expected. I will begin implementing the home page for the admin now

--- SenateVotingSystem/spec/requests/commissioner_spec.rb
+++ SenateVotingSystem/spec/requests/commissioner_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 RSpec.describe "Commissioners", type: :request do
   describe "GET #voting-results for commissioner" do
-    it 'should return hash table where key is the party name, and value is score' do
+    it 'should have an instance hash table where key is the party name, and value is score' do
       get '/admin/voting-results'
-      
+      scores= assigns(:party_votes)
+      expect(scores.key?("Labour".to_sym)).to eq(true)
+      expect(scores[:Labour]).to eq(0.0)
     end
     it 'should return top 10 candidates in descending order' do
-      
+      get '/admin/voting-results'
+      topCandidates= assigns(:candidates)
+      expect(topCandidates).to eq(Candidate.order(totalvotes: :desc).limit(10))
     end
   end
 end
