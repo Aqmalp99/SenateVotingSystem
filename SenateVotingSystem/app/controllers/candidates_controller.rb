4b486924f6b348d2d6e5f79735c42a871bd0e65c
a1747701 <a1747701@student.adelaide.edu.au>
2022-05-28 17:37:35 +0930

    multistep routing works

--- SenateVotingSystem/app/controllers/candidates_controller.rb
+++ SenateVotingSystem/app/controllers/candidates_controller.rb
@@ -4,5 +4,20 @@ class CandidatesController < ApplicationController
   end
 
   def add
+    @candidate = Candidate.new
+    print session[:candidate_step]
   end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+    @candidate.current_step = session[:candidate_step]
+    @candidate.next_step
+    session[:candidate_step] = @candidate.current_step
+    render 'add'
+  end
+
+  def candidate_params
+    params.require(:candidate).permit(:first_name, :surname, :party)
+  end
+
 end
