59a18af35602b5b9f06dda19f0de85277ab9b6b8
a1747701 <a1747701@student.adelaide.edu.au>
2022-05-29 15:18:11 +0930

    table of other candidates shows successfully. passes cucumber test

--- SenateVotingSystem/app/controllers/candidates_controller.rb
+++ SenateVotingSystem/app/controllers/candidates_controller.rb
@@ -4,12 +4,21 @@ class CandidatesController < ApplicationController
   end
 
   def add
+    session[:candidate_params] ||= {}
     @candidate = Candidate.new
+    @candidate.current_step = session[:candidate_step]
   end
 
   def create
+    session[:candidate_params].deep_merge!(candidate_params) if candidate_params
     @candidate = Candidate.new(candidate_params)
+    @candidate.current_step = session[:candidate_step]
     @candidate.next_step
+    session[:candidate_step] = @candidate.current_step
+    @other_candidates = Candidate.where('party = ?', @candidate.party) if session[:candidate_step] == 'order'
+    if !(@other_candidates.nil?)
+      print "#{@other_candidates.length}\n"
+    end
     render 'add'
   end
 
